cfe5cec7af6f95ef8b6b036fb2df53d63aab054e
borkarSahil <officialsahilborkar@gmail.com>
2023-09-16 12:35:06 +0530

    The Snake-Game

--- SnakeGame.py
+++ SnakeGame.py
@@ -7,6 +7,7 @@
 # TODO : DETECT THE COLLISION WITH TAIL
 
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -27,30 +28,17 @@ screen.title("Snake Game")
 # segment_3.color("white")
 
 #           APPROACH 2
-starting_positions = [(0,0), (-20,0), (-40,0)]
-segments = []
-
 screen.tracer(0)
 
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.5)
 
-    for seg_num in range(len(segments) -1, 0, -1):     # start = 2, stop = 0, step = -1
-        # In this we try to move 3rd segment to 2nd segment and 2nd segment
-        # to 1st segment and then move 1st segment
-        new_x = segments[seg_num -1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
+
 
 # def move_Up():
 #     new_segment.forward(10)
